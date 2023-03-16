6a8a27cb9641d3556784f649c7b390b2e479807f
jossydeks <maradesajoshua@gmail.com>
2023-03-16 08:45:00 -0700

    101-dalmatians

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - here is main function
+ *
+ * Return: failure value
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (-1);
+}
