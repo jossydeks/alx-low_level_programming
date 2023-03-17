12396a231c4b800e9785dc69e82096826bae75ce
jossydeks <maradesajoshua@gmail.com>
2023-03-17 09:41:55 -0700

    1to9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - numbers in base
+ *
+ * Return: 0 succesful run
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
