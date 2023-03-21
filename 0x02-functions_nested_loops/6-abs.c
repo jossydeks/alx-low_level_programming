0e4300af635c12685ac5016562e7ac14bdc09f49
jossydeks <maradesajoshua@gmail.com>
2023-03-21 08:41:12 -0700

    absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _abs - function for the absolute value of an integer
+ *
+ * @i: parameter of integer
+ *
+ * Return: always 0
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = -(i);
+	else if (i > 0)
+		i = i;
+	return (i);
+
+}
